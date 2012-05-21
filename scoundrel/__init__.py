141dd139be92950f6439307fd67e087bfa3040fe
Matt Dietz <matt.dietz@rackspace.com>
2012-05-21 01:02:44 -0500

    Added basic arrow key movement for testing

--- scoundrel/__init__.py
+++ scoundrel/__init__.py
@@ -17,33 +17,54 @@ class Scoundrel(object):
     def init(cls, conf):
         cls.context = scoundrel.engine.init(conf)
         cls.init_keymap(conf)
-        player = scoundrel.actor.player.PlayerActor()
-        cls.world = scoundrel.world.World(player)
+        player = scoundrel.actor.player.PlayerActor([15, 15])
+        cls.world = scoundrel.world.World(player, conf)
+
+        # Default values weren't working on a mac
+        pygame.key.set_repeat(100, 100)
         state_machine.playing()
 
     @classmethod
     def init_keymap(cls, conf):
         cls.keymap = {
-            pygame.locals.K_LEFT: cls.key_left,
-            pygame.locals.K_RIGHT: cls.key_right,
-            pygame.locals.K_UP: cls.key_up,
-            pygame.locals.K_DOWN: cls.key_down}
+            pygame.locals.K_LEFT: cls.key_arrow_left,
+            pygame.locals.K_RIGHT: cls.key_arrow_right,
+            pygame.locals.K_UP: cls.key_arrow_up,
+            pygame.locals.K_DOWN: cls.key_arrow_down,
+            pygame.locals.K_ESCAPE: cls.quit}
+
+    #TODO(cerberus): un-harcode the movement
+    @classmethod
+    def key_arrow_left(cls):
+        player = cls.world.player
+        player.position[0] -= 15
+        if player.position[0] < 15:
+            player.position[0] = 15
 
     @classmethod
-    def key_left(cls):
-        print "Left"
+    def key_arrow_right(cls):
+        player = cls.world.player
+        player.position[0] += 15
+        if player.position[0] > cls.world.conf['width']-15:
+            player.position[0] = cls.world.conf['width']-15
 
     @classmethod
-    def key_right(cls):
-        print "Right"
+    def key_arrow_up(cls):
+        player = cls.world.player
+        player.position[1] -= 15
+        if player.position[1] < 15:
+            player.position[1] = 15
 
     @classmethod
-    def key_up(cls):
-        print "Up"
+    def key_arrow_down(cls):
+        player = cls.world.player
+        player.position[1] += 15
+        if player.position[1] > cls.world.conf['height']-15:
+            player.position[1] = cls.world.conf['height']-15
 
     @classmethod
-    def key_down(cls):
-        print "Down"
+    def quit(cls):
+        raise StopExecution()
 
     @classmethod
     def handle_events(cls):
@@ -61,12 +82,22 @@ class Scoundrel(object):
     def draw(cls):
         scoundrel.engine.begin_draw(cls.context)
         cls.context.screen.fill(scoundrel.engine.colors['black'])
+        even = True
         for i in xrange(20):
+            even = not even
             for j in xrange(40):
-                rect = pygame.Rect(j*30, i*30, 20, 20)
-                pygame.draw.rect(cls.context.screen,
-                                 scoundrel.engine.colors['green'],
-                                 rect)
+                rect = pygame.Rect(j*30, i*30, 30, 30)
+                if even:
+                    pygame.draw.rect(cls.context.screen,
+                                     scoundrel.engine.colors['green'],
+                                     rect)
+                else:
+                    pygame.draw.rect(cls.context.screen,
+                                     scoundrel.engine.colors['white'],
+                                     rect)
+                    
+                even = not even
+        cls.world.draw(cls.context)
         scoundrel.engine.end_draw(cls.context)
 
     @classmethod
--- scoundrel/world/__init__.py
+++ scoundrel/world/__init__.py
@@ -1,9 +1,19 @@
+import pygame
+
+import scoundrel.engine
+
 class World(object):
-    def __init__(self, player):
+    def __init__(self, player, conf):
         self.actors = []
 
         # the player is an actor, but still a special one
         self.player = player
+        self.conf = conf
 
     def step(self, count=1):
-        print "World step"
+        pass
+
+    def draw(self, context):
+        #TODO(cerberus): un-hardcode the radius, store the tile width elsewhere
+        pygame.draw.circle(context.screen, scoundrel.engine.colors['red'],
+                           self.player.position, 15)
